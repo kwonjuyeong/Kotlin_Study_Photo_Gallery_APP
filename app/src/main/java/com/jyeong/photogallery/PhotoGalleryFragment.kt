2390e5d2c88538363ff6145fb2e4129e48de38a0
kwonjuyoung <20161527@vision.hoseo.edu>
2023-03-12 22:48:41 +0900

    Chapter27

--- app/src/main/java/com/jyeong/photogallery/PhotoGalleryFragment.kt
+++ app/src/main/java/com/jyeong/photogallery/PhotoGalleryFragment.kt
@@ -15,13 +15,12 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import androidx.work.Constraints
-import androidx.work.NetworkType
-import androidx.work.OneTimeWorkRequest
-import androidx.work.WorkManager
+import androidx.work.*
+import com.bignerdranch.android.photogallery.QueryPreferences
+import java.util.concurrent.TimeUnit
 
 private const val TAG = "PhotoGalleryFragment"
-
+private const val POLL_WORK = "POLL_WORK"
 class PhotoGalleryFragment : Fragment() {
 
     private lateinit var photoGalleryViewModel: PhotoGalleryViewModel
@@ -92,6 +91,15 @@ class PhotoGalleryFragment : Fragment() {
                 searchView.setQuery(photoGalleryViewModel.searchTerm, false)
             }
         }
+
+        val toggleItem = menu.findItem(R.id.menu_item_toggle_polling)
+        val isPolling = QueryPreferences.isPolling(requireContext())
+        val toggleItemTitle = if(isPolling){
+            R.string.stop_polling
+        }else{
+            R.string.start_polling
+        }
+        toggleItem.setTitle(toggleItemTitle)
     }
 
     //clear 선택 시 쿼리 삭제
@@ -100,6 +108,20 @@ class PhotoGalleryFragment : Fragment() {
             R.id.menu_item_clear ->{
                 photoGalleryViewModel.fetchPhotos("")
                 true
+            }
+            R.id.menu_item_toggle_polling -> {
+                val isPolling  = QueryPreferences.isPolling(requireContext())
+                if(isPolling){
+                    WorkManager.getInstance().cancelUniqueWork(POLL_WORK)
+                    QueryPreferences.setPolling(requireContext(), false)
+                }else{
+                    val constraints = Constraints.Builder().setRequiredNetworkType(NetworkType.UNMETERED).build()
+                    val periodicRequest = PeriodicWorkRequest.Builder(PollWorker::class.java, 15, TimeUnit.MINUTES).setConstraints(constraints).build()
+                    WorkManager.getInstance().enqueueUniquePeriodicWork(POLL_WORK, ExistingPeriodicWorkPolicy.KEEP, periodicRequest)
+                    QueryPreferences.setPolling(requireContext(), true)
+                }
+                activity?.invalidateOptionsMenu()
+                return true
             }
             else -> super.onOptionsItemSelected(item)
         }
